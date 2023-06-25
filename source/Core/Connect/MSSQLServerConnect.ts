bbc0e8de04eb398a8e33f8f64e1f23a3ed58e6fc
Никита Матвеев <nik.nikita.matveev.1997@gmail.com>
2023-06-25 14:20:11 +0500

    WIP

--- source/Core/Connect/MSSQLServerConnect.ts
+++ source/Core/Connect/MSSQLServerConnect.ts
@@ -1,13 +1,18 @@
 import { IConnect } from "./IConnect";
-
+import { connect, query  } from "mssql"
 
 class MSSQLServerConnect implements IConnect {
 
     OpenConnect(): void {
-        throw new Error("Method not implemented.");
+        connect("Server=localhost,1433;Database=master;User Id=sa;Password=StrongPassword123!@#")
+            .then((connectionPool) => {
+                console.log(connectionPool);
+            })
+            
     }
+
     CloseConnect(): void {
-        throw new Error("Method not implemented.");
+        
     }
 
 }
